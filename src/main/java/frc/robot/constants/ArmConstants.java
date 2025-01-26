2cc4d3e791b8b16ff59867044416f5f412afc669
Polar Pilots Programming Laptop 1 <DriverStation1_1498@outlook.com>
2025-01-26 13:26:24 -0500

    Writing commands for the arm.

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -1,5 +1,20 @@
 package frc.robot.constants;
 
 public class ArmConstants {
+public static final int kArmRotateCANID = 13;
+public static final int kEncoderCANID = 32;
+public static final double kStow = 0.0;
+public static final double kLoad = 0.1;
+public static final double kL1 = 0.3;
+public static final double kL2 = 0.4;
+public static final double kL3 = 0.5;
+public static final double kL4 = 0.6;
+public static final double kBarge = 0.9;
+public static final double kDeadband = 0.05;
+
+
+
+
+
 
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,14 +1,40 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ArmConstants;
 
 public class Arm extends SubsystemBase{
 
+    TalonFX ArmRotate;
+    CANcoder armRotateEncoder;
+    PositionVoltage RotateControl;
+
+
+
     public Arm() {
         //Constructor.
+        ArmRotate = new TalonFX(ArmConstants.kArmRotateCANID, "canivore");
+        armRotateEncoder = new CANcoder(ArmConstants.kEncoderCANID,"canivore");
+        RotateControl = new PositionVoltage(ArmConstants.kStow);
+
     }
-    
+    private void GoToPosition(double position) {
+            ArmRotate.setControl(RotateControl.withPosition(position));
+
+    }
+    private boolean IsArmAtPosition(double position) {
+            return ((position-ArmConstants.kDeadband) <= GetArmPosition()) && ((position+ArmConstants.kDeadband) >= GetArmPosition());
+    }
+    private double GetArmPosition(){
+            return ArmRotate.getPosition().getValueAsDouble();
+            
+    }
+
     @Override
     public void initSendable(SendableBuilder builder) {
         //Sendable data for dashboard debugging will be added here.
