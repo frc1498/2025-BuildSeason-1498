f2d4a78e033d022082a8e004cbe33aea9064273f
Trevor-G <Trevor.Goolsby@gmail.com>
2025-02-28 09:14:43 -0500

    Added auton selection code into the main branch.  Will need to test in sim.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,19 +6,24 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.events.EventTrigger;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
-
-//Was throwing an error
-//import frc.pilotLib.utility.Selector;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.pilotLib.utility.Selector;
 
 import frc.robot.config.CoralIntakeConfig;
 import frc.robot.config.ElevatorConfig;
@@ -83,15 +88,19 @@ public class RobotContainer {
 
     public LED leds = new LED();
 
-    public boolean hasDeterminedAlliance = false;
-    public DriverStation.Alliance allianceColor = Alliance.Blue;
-
     //Very important, the vision subsystem has to be created after the drivetrain.
     //The vision subsystem relies on creating a lambda that gets the drivetrain heading.
     public Vision vision = new Vision(() -> {return drivetrain.getPigeon2().getYaw().getValueAsDouble();});
 
-    // Was throwing an error.  had to comment out the import as well
-    // public Selector autoSelect = new Selector();
+    //Future proofing CHRP functionality.
+    //File chirpFolder = new File(Filesystem.getDeployDirectory() + "/chirp");
+    File autonFolder = new File(Filesystem.getDeployDirectory() + "/pathplanner/autos");
+    public Selector autoSelect = new Selector(autonFolder, ".auto", "Auto Selector");
+    //All of these are needed to display and load the correct list of autonomous options.
+    public ArrayList<Command> autonCommands = new ArrayList<Command>();
+    public Command selectedAuton;
+    public Alliance allianceColor = Alliance.Blue;
+    public boolean hasDeterminedAlliance;
 
     public RobotContainer() {
         configureBindings();
@@ -116,9 +125,18 @@ public class RobotContainer {
         //driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
         //driver.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
 
-        
-     
-
+        //Makes testing difficult, but sim makes it less annoying.
+        //Does three things:
+        //1. Filters the list of autons by alliance color.
+        //2. Loads all of the routines into auton commands that PathPlanner can run.
+        //3. Select the first auton in the list by default.
+        this.allianceCheck.onTrue(autoSelect.filterList(() -> {return allianceColor.toString();})
+            .andThen(() -> {autonCommands = this.loadAllAutonomous(autoSelect.currentList());}).ignoringDisable(true)
+            .andThen(() -> {selectedAuton = autonCommands.get(autoSelect.currentIndex().get());}).ignoringDisable(true));        
+    
+        //Auton Selection
+        driver.povLeft().onTrue(autoSelect.decrement().andThen(() -> {selectedAuton = autonCommands.get(autoSelect.currentIndex().get());}).ignoringDisable(true));
+        driver.povRight().onTrue(autoSelect.increment().andThen(() -> {selectedAuton = autonCommands.get(autoSelect.currentIndex().get());}).ignoringDisable(true));            
 
         //===================================================================================
         //=============================Driver Commands=======================================
@@ -199,6 +217,17 @@ public class RobotContainer {
 
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return selectedAuton;
     }
+
+    public ArrayList<Command> loadAllAutonomous(Supplier<ArrayList<String>> autonList) {
+        ArrayList<Command> commandList= new ArrayList<Command>();
+        for (var i : autonList.get()) {
+            commandList.add(new PathPlannerAuto(i));
+        }
+
+        return commandList;
+    }
+
+    public Trigger allianceCheck = new Trigger(() -> {return this.hasDeterminedAlliance;});
 }
