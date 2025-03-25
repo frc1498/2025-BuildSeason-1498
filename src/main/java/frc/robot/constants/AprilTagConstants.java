198a773b4a455f90e490222c46cd487f5345bb84
Trevor-G <Trevor.Goolsby@gmail.com>
2025-03-24 22:18:10 -0400

    Added reef positions to the constants file.

--- src/main/java/frc/robot/constants/AprilTagConstants.java
+++ src/main/java/frc/robot/constants/AprilTagConstants.java
@@ -4,5 +4,29 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class AprilTagConstants {
-    public static final Pose2d kRed6 = new Pose2d(13.47, 3.30, Rotation2d.fromDegrees(300));
+    public static final Pose2d kRedTag6Left = new Pose2d(13.592, 2.896, Rotation2d.fromDegrees(120)); //K
+    public static final Pose2d kRedTag6Right = new Pose2d(13.893, 3.02, Rotation2d.fromDegrees(120)); //L
+    public static final Pose2d kRedTag7Left = new Pose2d(14.332, 3.892, Rotation2d.fromDegrees(180)); //A
+    public static final Pose2d kRedTag7Right = new Pose2d(14.332, 4.221, Rotation2d.fromDegrees(180)); //B
+    public static final Pose2d kRedTag8Left = new Pose2d(13.845, 5.016, Rotation2d.fromDegrees(-120)); //C
+    public static final Pose2d kRedTag8Right = new Pose2d(13.578, 5.195, Rotation2d.fromDegrees(-120)); //D
+    public static final Pose2d kRedTag9Left = new Pose2d(12.588, 5.208, Rotation2d.fromDegrees(-60)); //E
+    public static final Pose2d kRedTag9Right = new Pose2d(12.341, 5.043, Rotation2d.fromDegrees(-60)); //F
+    public static final Pose2d kRedTag10Left = new Pose2d(11.786, 4.206, Rotation2d.fromDegrees(0)); //G
+    public static final Pose2d kRedTag10Right = new Pose2d(11.806, 3.843, Rotation2d.fromDegrees(0)); //H
+    public static final Pose2d kRedTag11Left = new Pose2d(12.341, 2.999, Rotation2d.fromDegrees(60)); //I
+    public static final Pose2d kRedTag11Right = new Pose2d(12.616, 2.841, Rotation2d.fromDegrees(60)); //J
+
+    public static final Pose2d kBlueTag17Left = new Pose2d(3.755, 2.977, Rotation2d.fromDegrees(60)); //C
+    public static final Pose2d kBlueTag17Right = new Pose2d(4.055, 2.809, Rotation2d.fromDegrees(60)); //D
+    public static final Pose2d kBlueTag18Left = new Pose2d(3.215, 4.162, Rotation2d.fromDegrees(0)); //A
+    public static final Pose2d kBlueTag18Right = new Pose2d(3.228, 3.792, Rotation2d.fromDegrees(0)); //B
+    public static final Pose2d kBlueTag19Left = new Pose2d(3.923, 5.218, Rotation2d.fromDegrees(-60)); //K
+    public static final Pose2d kBlueTag19Right = new Pose2d(3.695, 5.050, Rotation2d.fromDegrees(-60)); //L
+    public static final Pose2d kBlueTag20Left = new Pose2d(5.220, 5.000, Rotation2d.fromDegrees(-120)); //I
+    public static final Pose2d kBlueTag20Right = new Pose2d(4.906, 5.242, Rotation2d.fromDegrees(-120)); //J
+    public static final Pose2d kBlueTag21Left = new Pose2d(5.732, 3.912, Rotation2d.fromDegrees(180)); //G
+    public static final Pose2d kBlueTag21Right = new Pose2d(5.780, 4.259, Rotation2d.fromDegrees(180)); //H
+    public static final Pose2d kBlueTag22Left = new Pose2d(4.942, 2.869, Rotation2d.fromDegrees(120)); //E
+    public static final Pose2d kBlueTag22Right = new Pose2d(5.277, 3.049, Rotation2d.fromDegrees(120)); //F
 }
